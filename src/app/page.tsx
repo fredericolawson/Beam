456fcddec3ff799bc5cf9b6cd5f029f83c5985d0
Frederick Lawson <fredjlawson@gmail.com>
2025-05-19 17:27:12 -0300

    minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,28 +31,52 @@ function Products() {
   return (
     <div className="w-full flex flex-col gap-8">
       <div>
-      <h2 className="heading-2">Technology</h2>
-      <ul>
-        <LinkItem href="https://taxis.beam.bm/" icon={<Car />}>Beam Taxis</LinkItem>
+        <h2 className="heading-2">Technology</h2>
+        <ul>
+          <LinkItem href="https://taxis.beam.bm/" icon={<Car />}>
+            Beam Taxi
+          </LinkItem>
         </ul>
       </div>
       <div>
         <h3 className="heading-2">Resources</h3>
         <ul className="flex flex-col gap-2">
-          <LinkItem href="https://weather.beam.bm/" icon={<Radar />}>Weather</LinkItem>
-          <LinkItem href="https://bermudaferries.com/" icon={<Ship />}>Bermuda Ferries</LinkItem>
-          <LinkItem href="https://www.whatson.bm/" icon={<Calendar />}>What&#39;s On Bermuda</LinkItem>
-          <LinkItem href="https://startup-drinks.com/bermuda" icon={<Martini />}>Startup Drinks</LinkItem>
+          <LinkItem href="https://weather.beam.bm/" icon={<Radar />}>
+            Weather
+          </LinkItem>
+          <LinkItem href="https://bermudaferries.com/" icon={<Ship />}>
+            Bermuda Ferries
+          </LinkItem>
+          <LinkItem href="https://www.whatson.bm/" icon={<Calendar />}>
+            What&#39;s On Bermuda
+          </LinkItem>
+          <LinkItem
+            href="https://startup-drinks.com/bermuda"
+            icon={<Martini />}
+          >
+            Startup Drinks
+          </LinkItem>
         </ul>
       </div>
     </div>
   );
 }
 
-function LinkItem({ href, children, icon }: { href: string; children: React.ReactNode; icon: React.ReactNode }) {
+function LinkItem({
+  href,
+  children,
+  icon,
+}: {
+  href: string;
+  children: React.ReactNode;
+  icon: React.ReactNode;
+}) {
   return (
     <li>
-      <Link href={href} className="rounded-md bg-background text-primary border border-primary p-2 flex items-center gap-2  hover:text-accent hover:border hover:border-accent transition-all duration-200">
+      <Link
+        href={href}
+        className="rounded-md bg-background text-primary border border-primary p-2 flex items-center gap-2  hover:text-accent hover:border hover:border-accent transition-all duration-200"
+      >
         {icon}
         {children}
       </Link>
